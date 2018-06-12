79f1094690f18b24b49023c966172ca8f49d27da
Sven Eckelmann <sven@narfation.org>
2018-06-12 22:10:57 +0200

    batman-adv: add patches from 2018.1-maint 2018-06-12
    * compat: initialize sinfo in cfg80211_get_station
    * Avoid storing non-TT-sync flags on singular entries too
    * Fix multicast TT issues with bogus ROAM flags
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- batman-adv/files/compat-hacks.h
+++ batman-adv/files/compat-hacks.h
@@ -204,3 +204,22 @@ static inline int batadv_nla_put_u64_64bit(struct sk_buff *skb, int attrtype,
 #define __ro_after_init
 
 #endif /* < KERNEL_VERSION(4, 10, 0) */
+
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
+
+#include <net/cfg80211.h>
+
+/* cfg80211 fix: https://patchwork.kernel.org/patch/10449857/ */
+static inline int batadv_cfg80211_get_station(struct net_device *dev,
+					      const u8 *mac_addr,
+					      struct station_info *sinfo)
+{
+	memset(sinfo, 0, sizeof(*sinfo));
+	return cfg80211_get_station(dev, mac_addr, sinfo);
+}
+
+#define cfg80211_get_station(dev, mac_addr, sinfo) \
+	batadv_cfg80211_get_station(dev, mac_addr, sinfo)
+
+#endif /* < KERNEL_VERSION(4, 18, 0) */

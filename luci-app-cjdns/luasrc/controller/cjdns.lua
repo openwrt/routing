088b62bac09550ef062876f6d9aad014c0f498c6
William Fleurant <william@netblazr.com>
2018-07-02 02:02:40 +0000

    luci-app-cjdns: bump cjdns and update luci admin
    - cjdns: bump to crashey @ a88859364
    - luci-app-cjdns: adds layer 2 and 3 address display column
    - luci-app-cjdns: fixes switch label pinger
    - luci-app-cjdns: get version from addr string
    - luci-app-cjdns: parse addr string for peerStats publicKey

--- luci-app-cjdns/luasrc/controller/cjdns.lua
+++ luci-app-cjdns/luasrc/controller/cjdns.lua
@@ -51,7 +51,9 @@ function act_peers()
 		end
 
 		for i,peer in pairs(response.peers) do
-			peer.ipv6 = publictoip6(peer.publicKey)
+			peer.ipv6 = publictoip6(peer.addr)
+			peer.version = pstatver(peer.addr)
+			peer.label = pstatlabel(peer.addr)
 			if peer.user == nil then
 				peer.user = ''
 				uci.cursor():foreach("cjdns", "udp_peer", function(udp_peer)
@@ -97,9 +99,23 @@ function act_ping()
 	luci.http.write_json(response)
 end
 
-function publictoip6(publicKey)
-	local process = io.popen("/usr/bin/publictoip6 " .. publicKey, "r")
+function publictoip6(addrLine)
+	local t = string.sub(addrLine, string.len(addrLine) - 53)
+	local process = io.popen("/usr/bin/publictoip6 " .. t, "r")
 	local ipv6    = process:read()
 	process:close()
 	return ipv6
+end
+
+function pstatver(addrLine)
+	for str in string.gmatch(addrLine, "([^"..".".."]+)") do
+		if str then return str else return '-' end
+	end
+end
+
+function pstatlabel(addrLine)
+	local t={}; local i=0; for str in string.gmatch(addrLine, "([^"..".".."]+)") do
+		if i >= 1 and i <= 4 then t[#t+1] = str end i = i + 1
+	end
+	return table.concat(t, ".")
 end

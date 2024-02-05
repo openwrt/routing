openwrt/packages

commit f5205d7d24342f78a5526d23b5437af2de44d91e

batman-adv: upgrade package to latest release 2014.2.0
 * remove batctl tool into separate package
 * support for vlan options added

Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

batman-adv/Makefile
@@ -10,17 +10,14 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=batman-adv
 
-PKG_VERSION:=2014.1.0
-BATCTL_VERSION:=2014.1.0
-PKG_RELEASE:=3
-PKG_MD5SUM:=d55aabb2bc508194407187648c773c1b
-BATCTL_MD5SUM:=92fd4cb60e38ec952fb5fdda4b2e6933
+PKG_VERSION:=2014.2.0
+PKG_RELEASE:=1
+PKG_MD5SUM:=1243029b3a3e2f4fa721d1a59c2faaf5
 
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION).tar.gz
 PKG_SOURCE_URL:=http://downloads.open-mesh.org/batman/releases/batman-adv-$(PKG_VERSION)
 
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)/$(PKG_NAME)-$(PKG_VERSION)
-PKG_BATCTL_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)/batctl-$(BATCTL_VERSION)
 
 include $(INCLUDE_DIR)/package.mk
 include $(INCLUDE_DIR)/kernel.mk
@@ -29,7 +26,7 @@ define KernelPackage/batman-adv
   URL:=http://www.open-mesh.org/
   MAINTAINER:=Marek Lindner <mareklindner@neomailbox.ch>
   SUBMENU:=Network Support
-  DEPENDS:=+KMOD_BATMAN_ADV_BLA:kmod-lib-crc16 +kmod-crypto-core +kmod-crypto-crc32c +kmod-lib-crc32c +libc +libnl-tiny
+  DEPENDS:=+KMOD_BATMAN_ADV_BLA:kmod-lib-crc16 +kmod-crypto-core +kmod-crypto-crc32c +kmod-lib-crc32c
   TITLE:=B.A.T.M.A.N. Adv
   FILES:=$(PKG_BUILD_DIR)/batman-adv.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,50,batman-adv)
@@ -37,9 +34,8 @@ endef
 
 define KernelPackage/batman-adv/description
 B.A.T.M.A.N. advanced is a kernel module which allows to
-build layer 2 mesh networks. This package contains the
-version $(PKG_VERSION) of the kernel module plus its user space
-configuration & managerment tool batctl.
+build layer 2 mesh networks. This package builds
+version $(PKG_VERSION) of the kernel module.
 endef
 
 define KernelPackage/batman-adv/config
@@ -57,63 +53,9 @@ MAKE_BATMAN_ADV_ARGS += \
 	CONFIG_BATMAN_ADV_DEBUG=$(if $(CONFIG_KMOD_BATMAN_ADV_DEBUG_LOG),y,n) \
 	CONFIG_BATMAN_ADV_BLA=$(if $(CONFIG_KMOD_BATMAN_ADV_BLA),y,n) \
 	CONFIG_BATMAN_ADV_DAT=$(if $(CONFIG_KMOD_BATMAN_ADV_DAT),y,n) \
+	CONFIG_BATMAN_ADV_MCAST=$(if $(CONFIG_KMOD_BATMAN_ADV_MCAST),y,n) \
 	CONFIG_BATMAN_ADV_NC=$(if $(CONFIG_KMOD_BATMAN_ADV_NC),y,n) \
 	REVISION="" all
-
-# The linker can identify unused sections of a binary when each symbol is stored
-# in a separate section. This mostly removes unused linker sections and reduces 
-# the size by ~3% on mipsel.
-
-TARGET_CFLAGS  += -ffunction-sections -fdata-sections
-TARGET_LDFLAGS += -Wl,--gc-sections
-
-# Link-time optimization allows to move parts of the optimization from the single
-# source file to the global source view. This is done by emitting the GIMPLE
-# representation in each object file and analyzing it again during the link step.
-
-TARGET_CFLAGS  += -flto
-TARGET_LDFLAGS += -fuse-linker-plugin
-
-MAKE_BATCTL_ENV += \
-	CPPFLAGS="$(TARGET_CPPFLAGS)" \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="$(TARGET_LDFLAGS)" \
-	LIBNL_NAME="libnl-tiny"
-
-MAKE_BATCTL_ARGS += \
-	REVISION="" \
-	CC="$(TARGET_CC)" \
-	DESTDIR="$(PKG_INSTALL_DIR)" \
-	batctl install
-
-ifneq ($(DEVELOPER)$(CONFIG_KMOD_BATMAN_ADV_BATCTL),)
-define Download/batctl
-  FILE:=batctl-$(BATCTL_VERSION).tar.gz
-  URL:=$(PKG_SOURCE_URL)
-  MD5SUM:=$(BATCTL_MD5SUM)
-endef
-$(eval $(call Download,batctl))
-
-BATCTL_EXTRACT = tar xzf "$(DL_DIR)/batctl-$(BATCTL_VERSION).tar.gz" -C "$(BUILD_DIR)/$(PKG_NAME)"
-BATCTL_PATCH = $(call Build/DoPatch,"$(PKG_BATCTL_BUILD_DIR)","$(PATCH_DIR)","*batctl*")
-BATCTL_BUILD = $(MAKE_BATCTL_ENV) $(MAKE) -C $(PKG_BATCTL_BUILD_DIR) $(MAKE_BATCTL_ARGS)
-BATCTL_INSTALL = $(INSTALL_BIN) $(PKG_INSTALL_DIR)/usr/local/sbin/batctl $(1)/usr/sbin/
-endif
-
-KPATCH ?= $(PATCH)
-define Build/DoPatch
-	@if [ -d "$(2)" ]; then \
-		if [ "$$$$(ls $(2) | grep -Ec $(3))" -gt 0 ]; then \
-			$(KPATCH) "$(1)" "$(2)" "$(3)"; \
-		fi; \
-	fi
-endef
-
-define Build/Patch
-	$(call Build/DoPatch,"$(PKG_BUILD_DIR)","$(PATCH_DIR)","*batman*")
-	$(BATCTL_EXTRACT)
-	$(BATCTL_PATCH)
-endef
 
 define Build/Prepare
 	$(call Build/Prepare/Default)
@@ -121,10 +63,8 @@ define Build/Prepare
 		$(PKG_BUILD_DIR)/main.h
 endef
 
-
 define Build/Compile
 	$(MAKE) -C "$(PKG_BUILD_DIR)" $(MAKE_BATMAN_ADV_ARGS)
-	$(BATCTL_BUILD)
 endef
 
 define Build/Clean
@@ -137,7 +77,7 @@ define KernelPackage/batman-adv/install
 	$(INSTALL_DATA) ./files/lib/batman-adv/config.sh $(1)/lib/batman-adv
 	$(INSTALL_BIN) ./files/etc/hotplug.d/net/99-batman-adv $(1)/etc/hotplug.d/net
 	$(INSTALL_BIN) ./files/lib/netifd/proto/batadv.sh $(1)/lib/netifd/proto
-	$(BATCTL_INSTALL)
+	$(INSTALL_BIN) ./files/lib/netifd/proto/batadv_vlan.sh $(1)/lib/netifd/proto
 endef
 
 $(eval $(call KernelPackage,batman-adv))

batman-adv/files/lib/netifd/proto/batadv_vlan.sh
@@ -1,0 +1,29 @@
+#!/bin/sh
+
+. /lib/functions.sh
+. ../netifd-proto.sh
+init_proto "$@"
+
+proto_batadv_vlan_init_config() {
+	proto_config_add_string "ap_isolation"
+}
+
+proto_batadv_vlan_setup() {
+	local config="$1"
+	local iface="$2"
+
+	# VLAN specific variables
+	local device="${iface%.*}"
+	local vid="${iface#*.}"
+
+	# batadv_vlan options
+	local ap_isolation
+
+	json_get_vars ap_isolation
+
+	echo "$ap_isolation" > "/sys/class/net/${device}/mesh/vlan${vid}/ap_isolation"
+	proto_init_update "$iface" 1
+	proto_send_update "$config"
+}
+
+add_protocol batadv_vlan

batman-adv/Config.in
@@ -14,16 +14,13 @@ config KMOD_BATMAN_ADV_DAT
 	depends on PACKAGE_kmod-batman-adv
 	default y
 
+config KMOD_BATMAN_ADV_MCAST
+	bool "enable multicast transmission optimization"
+	depends on PACKAGE_kmod-batman-adv
+	default y
+
 config KMOD_BATMAN_ADV_NC
 	bool "enable network coding [requires promisc mode support]"
 	depends on PACKAGE_kmod-batman-adv
 	default n
 
-config KMOD_BATMAN_ADV_BATCTL
-	bool "enable batctl"
-	depends on PACKAGE_kmod-batman-adv
-	default y
-	help
-	  batctl is a more intuitive managment utility for B.A.T.M.A.N.-Advanced.
-	  It is an easier method for configuring batman-adv and
-	  provides some additional tools for debugging as well.

batman-adv/files/etc/config/batman-adv
@@ -12,6 +12,7 @@ config 'mesh' 'bat0'
 	option 'vis_mode'
 	option 'bridge_loop_avoidance'
 	option 'distributed_arp_table'
+	option 'multicast_mode'
 	option 'network_coding'
 	option 'hop_penalty'
 	option 'isolation_mark'

batman-adv/files/lib/batman-adv/config.sh
@@ -26,6 +26,7 @@ bat_config()
 	config_get gw_sel_class "$mesh" gw_sel_class
 	config_get hop_penalty "$mesh" hop_penalty
 	config_get isolation_mark "$mesh" isolation_mark
+	config_get multicast_mode "$mesh" multicast_mode
 	config_get network_coding "$mesh" network_coding
 	config_get log_level "$mesh" log_level
 	config_get orig_interval "$mesh" orig_interval
@@ -44,6 +45,7 @@ bat_config()
 	[ -n "$gw_sel_class" ] && echo $gw_sel_class > /sys/class/net/$mesh/mesh/gw_sel_class
 	[ -n "$hop_penalty" ] && echo $hop_penalty > /sys/class/net/$mesh/mesh/hop_penalty
 	[ -n "$isolation_mark" ] && echo $isolation_mark > /sys/class/net/$mesh/mesh/isolation_mark
+	[ -n "$multicast_mode" ] && echo $multicast_mode > /sys/class/net/$mesh/mesh/multicast_mode 2>&-
 	[ -n "$network_coding" ] && echo $network_coding > /sys/class/net/$mesh/mesh/network_coding 2>&-
 	[ -n "$log_level" ] && echo $log_level > /sys/class/net/$mesh/mesh/log_level 2>&-
 	[ -n "$orig_interval" ] && echo $orig_interval > /sys/class/net/$mesh/mesh/orig_interval

batman-adv/patches/0001-batman-adv-fix-neigh_ifinfo-imbalance.patch
@@ -1,35 +1,0 @@
-From a424cd5a2c956ef1f0353d7e5f2b7fbc4af7d2d8 Mon Sep 17 00:00:00 2001
-From: Simon Wunderlich <simon@open-mesh.com>
-Date: Wed, 26 Mar 2014 15:46:21 +0100
-Subject: [PATCH 1/9] batman-adv: fix neigh_ifinfo imbalance
-
-The neigh_ifinfo object must be freed if it has been used in
-batadv_iv_ogm_process_per_outif().
-
-This is a regression introduced by
-9bb33b8d88e318c4879d37d06ad28e3e018b9036 ("batman-adv: split tq
-information in neigh_node struct")
-
-Reported-by: Antonio Quartulli <antonio@open-mesh.com>
-Signed-off-by: Simon Wunderlich <simon@open-mesh.com>
-Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
----
- bat_iv_ogm.c | 2 ++
- 1 file changed, 2 insertions(+)
-
-diff --git a/bat_iv_ogm.c b/bat_iv_ogm.c
-index 8323bce..d074d06 100644
---- a/bat_iv_ogm.c
-+++ b/bat_iv_ogm.c
-@@ -1545,6 +1545,8 @@ out_neigh:
- 	if ((orig_neigh_node) && (!is_single_hop_neigh))
- 		batadv_orig_node_free_ref(orig_neigh_node);
- out:
-+	if (router_ifinfo)
-+		batadv_neigh_ifinfo_free_ref(router_ifinfo);
- 	if (router)
- 		batadv_neigh_node_free_ref(router);
- 	if (router_router)
--- 
-2.0.0.rc2
-

batman-adv/patches/0002-batman-adv-fix-neigh-reference-imbalance.patch
@@ -1,47 +1,0 @@
-From cdd09f69871ce8c98b8ae9fa0583f73938768943 Mon Sep 17 00:00:00 2001
-From: Simon Wunderlich <simon@open-mesh.com>
-Date: Wed, 26 Mar 2014 15:46:22 +0100
-Subject: [PATCH 2/9] batman-adv: fix neigh reference imbalance
-
-When an interface is removed from batman-adv, the orig_ifinfo of a
-orig_node may be removed without releasing the router first.
-This will prevent the reference for the neighbor pointed at by the
-orig_ifinfo->router to be released, and this leak may result in
-reference leaks for the interface used by this neighbor. Fix that.
-
-This is a regression introduced by
-de6bcc76ea84fecb136f8c8f5ba1862e4a13f06b ("batman-adv: split out router
-from orig_node").
-
-Reported-by: Antonio Quartulli <antonio@open-mesh.com>
-Signed-off-by: Simon Wunderlich <simon@open-mesh.com>
-Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
----
- originator.c | 5 +++++
- 1 file changed, 5 insertions(+)
-
-diff --git a/originator.c b/originator.c
-index 8539416..25df60d 100644
---- a/originator.c
-+++ b/originator.c
-@@ -500,12 +500,17 @@ batadv_neigh_node_get(const struct batadv_orig_node *orig_node,
- static void batadv_orig_ifinfo_free_rcu(struct rcu_head *rcu)
- {
- 	struct batadv_orig_ifinfo *orig_ifinfo;
-+	struct batadv_neigh_node *router;
- 
- 	orig_ifinfo = container_of(rcu, struct batadv_orig_ifinfo, rcu);
- 
- 	if (orig_ifinfo->if_outgoing != BATADV_IF_DEFAULT)
- 		batadv_hardif_free_ref_now(orig_ifinfo->if_outgoing);
- 
-+	/* this is the last reference to this object */
-+	router = rcu_dereference_protected(orig_ifinfo->router, true);
-+	if (router)
-+		batadv_neigh_node_free_ref_now(router);
- 	kfree(orig_ifinfo);
- }
- 
--- 
-2.0.0.rc2
-

batman-adv/patches/0003-batman-adv-always-run-purge_orig_neighbors.patch
@@ -1,49 +1,0 @@
-From 72125152cc46e55793329984428032769648904e Mon Sep 17 00:00:00 2001
-From: Simon Wunderlich <simon@open-mesh.com>
-Date: Wed, 26 Mar 2014 15:46:23 +0100
-Subject: [PATCH 3/9] batman-adv: always run purge_orig_neighbors
-
-The current code will not execute batadv_purge_orig_neighbors() when an
-orig_ifinfo has already been purged. However we need to run it in any
-case. Fix that.
-
-This is a regression introduced by
-de6bcc76ea84fecb136f8c8f5ba1862e4a13f06b ("batman-adv: split out router
-from orig_node")
-
-Signed-off-by: Simon Wunderlich <simon@open-mesh.com>
-Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
----
- originator.c | 8 ++++----
- 1 file changed, 4 insertions(+), 4 deletions(-)
-
-diff --git a/originator.c b/originator.c
-index 25df60d..47b0886 100644
---- a/originator.c
-+++ b/originator.c
-@@ -857,7 +857,7 @@ static bool batadv_purge_orig_node(struct batadv_priv *bat_priv,
- {
- 	struct batadv_neigh_node *best_neigh_node;
- 	struct batadv_hard_iface *hard_iface;
--	bool changed;
-+	bool changed_ifinfo, changed_neigh;
- 
- 	if (batadv_has_timed_out(orig_node->last_seen,
- 				 2 * BATADV_PURGE_TIMEOUT)) {
-@@ -867,10 +867,10 @@ static bool batadv_purge_orig_node(struct batadv_priv *bat_priv,
- 			   jiffies_to_msecs(orig_node->last_seen));
- 		return true;
- 	}
--	changed = batadv_purge_orig_ifinfo(bat_priv, orig_node);
--	changed = changed || batadv_purge_orig_neighbors(bat_priv, orig_node);
-+	changed_ifinfo = batadv_purge_orig_ifinfo(bat_priv, orig_node);
-+	changed_neigh = batadv_purge_orig_neighbors(bat_priv, orig_node);
- 
--	if (!changed)
-+	if (!changed_ifinfo && !changed_neigh)
- 		return false;
- 
- 	/* first for NULL ... */
--- 
-2.0.0.rc2
-

openwrt/routing

commit 5a7cac9b4ecf78c686fd2afeacec591cecf60a6b

batman-adv: Provide teardown hook for batadv_vlan proto
The batadv_vlan proto doesn't need to do anything when it gets teared down.
But the scripts are still trying to call the teardown function of this
proto. This results in warnings like:

    daemon.notice netifd: batmesh1 (18940): ./batadv_vlan.sh: eval: line 37: proto_batadv_vlan_teardown: not found

Just providing a stub function avoids this log spam.

Fixes: #1044
Reported-by: Rani Hod <rani.hod@gmail.com>
Fixes: f5205d7d2434 ("batman-adv: upgrade package to latest release 2014.2.0")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

batman-adv/Makefile
@@ -4,7 +4,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=batman-adv
 PKG_VERSION:=2023.1
-PKG_RELEASE:=5
+PKG_RELEASE:=6
 
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION).tar.gz
 PKG_SOURCE_URL:=https://downloads.open-mesh.org/batman/releases/batman-adv-$(PKG_VERSION)

batman-adv/files/lib/netifd/proto/batadv_vlan.sh
@@ -22,4 +22,8 @@ proto_batadv_vlan_setup() {
 	proto_send_update "$config"
 }
 
+proto_batadv_vlan_teardown() {
+	local cfg="$1"
+}
+
 add_protocol batadv_vlan
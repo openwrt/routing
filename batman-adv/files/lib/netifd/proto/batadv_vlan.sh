b6b1ef9e1e73a2d0d491a46507e7db3d96c814ec
wififreedom <265249790+wififreedom@users.noreply.github.com>
2026-03-07 23:59:36 +0100

    Fix broken support for configuration of batman-adv VLAN-level AP isolation.
    Old syntax required by batadv_vlan.sh for /etc/config/network:
    
    config interface 'bat0.40'
            option proto 'batadv_vlan'
            option ap_isolation '1'
    
    This was broken:
    - An interface name cannot contain a '.' (dot), so the 'uci' tool
      could not parse this syntax.
    - Configuration changes to the interface were not applied on
      execution of 'service network reload', and possibly in other cases,
      because the interface did not have a device associated with it.
    
    New syntax for /etc/config/network:
    
    config interface 'bat0_40'
            option device 'bat0.40'
            option proto 'batadv_vlan'
            option ap_isolation '1'
    
    Also, as in other /lib/netifd/proto scripts, take into account
    $INCLUDE_ONLY.

--- batman-adv/files/lib/netifd/proto/batadv_vlan.sh
+++ batman-adv/files/lib/netifd/proto/batadv_vlan.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-. /lib/functions.sh
-. ../netifd-proto.sh
-init_proto "$@"
+[ -n "$INCLUDE_ONLY" ] || {
+	. /lib/functions.sh
+	. ../netifd-proto.sh
+	init_proto "$@"
+}
 
 proto_batadv_vlan_init_config() {
+	proto_config_add_string 'device'
 	proto_config_add_boolean 'ap_isolation:bool'
 }
 
@@ -13,11 +16,17 @@ proto_batadv_vlan_setup() {
 	local iface="$2"
 
 	# batadv_vlan options
+	local device
 	local ap_isolation
 
+	json_get_vars device
 	json_get_vars ap_isolation
 
-	[ -n "$ap_isolation" ] && batctl vlan "$iface" ap_isolation "$ap_isolation"
+	batadv_iface="${device%.*}"
+
+	( proto_add_host_dependency "$config" '' "$batadv_iface" )
+
+	batctl vlan "$device" ap_isolation "${ap_isolation:-0}"
 	proto_init_update "$iface" 1
 	proto_send_update "$config"
 }
